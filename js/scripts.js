62d099b00eec77fb615b5e64efda1e4fa9832f9d
Kevin <kevinambundo3@gmail.com>
2020-07-26 22:07:00 +0300

    Add readme file

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#Designlogo").click(function() {
+      $(".design").toggle();
+    });
+  });
+  $(document).ready(function(){
+    $("#developmentlogo").click(function() {
+      $(".development").toggle();
+    });
+  });
+  $(document).ready(function(){
+    $("#productlogo").click(function() {
+      $(".product").toggle();
+    });
+  });
